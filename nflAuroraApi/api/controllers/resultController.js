bbcce6ae2791281ce9d31426bad8589af5066d8c
Samuel Jost <samuel.jost.1@post.ch>
2019-08-20 17:23:13 +0200

    Able to detect changes, missing the call to Aurora

--- nflAuroraApi/api/controllers/resultController.js
+++ nflAuroraApi/api/controllers/resultController.js
@@ -2,12 +2,13 @@
 var mongoose = require('mongoose'),
  Score = mongoose.model('Score');
 
-const GameScore = require('../models/resultModel.js');
 exports.list_all_games = function(req, res){
   console.log('Get Request incoming: List all Games');
   Score.find({}, function(err, score){
     if(err)
       res.send(err);
+
+    console.log(score);
     res.json(score);
   });
 };
@@ -15,12 +16,27 @@ exports.list_all_games = function(req, res){
 exports.add_game = function(req, res){
   console.log('Post Request to add Game');
   var newScore = new Score(req.body);
-  newScore.save(function(err, score){
-    if(err)
-      res.send(err);
-    res.json(score);
-  });
-  var newGame = new GameScore(req.body);
-  console.log(newGame.toString());
-  res.json({});
+  Score.findOneAndUpdate(
+    {
+      hometeam:req.body.hometeam,
+      awayteam:req.body.awayteam
+    }, req.body, {upsert:true}, function(err, score) {
+      if(score){
+        if(req.body.homescore + req.body.awayscore != score.homescore + score.awayscore){
+          console.log("was updated");
+          //Send Request to Nanoleaf
+        }
+
+        console.log(score.hometeam+": +"+(req.body.homescore - score.homescore));
+        console.log(score.awayteam+": +"+(req.body.awayscore - score.awayscore));
+
+      } else {
+        console.log("new game");
+        newScore.save(function(err, score){
+          if(err)
+            res.send(err);
+        });
+      }
+      res.json(newScore);
+    });
 };
