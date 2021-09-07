adb16e03f9878888299350e0848f8acaae1aec07
jostsa <samuel.jost.1@post.ch>
2021-09-07 16:37:56 +0200

    cleanup for new season 2021

--- nflAuroraApi/api/routes/resultRoutes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function(app){
-  var resultList = require('../controllers/resultController');
-  app.route('/results')
-    .get(resultList.list_all_games)
-    .post(resultList.add_game)
-    .delete(resultList.clean_old_games);
-}
